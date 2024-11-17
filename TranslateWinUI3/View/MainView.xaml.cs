87098c2d7700865d2415b62ab6707ea2d2cb386f
张威年 <zhangweinian.cqupt@outlook.com>
2024-11-17 20:25:57 +0800

    重构UI布局，添加本地化和翻译功能
    在 `BaiduAPI.vcxproj` 文件中，修改了生成后事件的命令，增加了将生成的 DLL 文件复制到 `TranslateWinUI3\Import` 目录的步骤。
    
    在 `Package.appxmanifest` 文件中，修改了应用程序的显示名称和描述，新的显示名称为“我要学外语 ！！！”，新的描述为“✌️我要学外语✌️”。
    
    在 `TranslateWinUI3.csproj` 文件中，增加了默认语言设置为中文（`zh-cn`）和允许不安全代码块的配置。同时，增加了资源文件 `Resources.resx` 的嵌入和设计时生成配置。
    
    在 `MainView.xaml` 文件中，重构了 UI 布局，移除了原来的 `StackPanel` 布局，增加了自定义标题栏和输入、输出文本框的布局。
    
    在 `MainView.xaml.cs` 文件中，增加了 `MainViewModel` 的初始化，设置了窗口的默认大小，隐藏了原生标题栏并设置了自定义标题栏。增加了语言切换按钮的点击事件处理逻辑和翻译按钮的点击事件处理逻辑。
    
    在 `Resources.Designer.cs` 文件中，添加了一个自动生成的资源类 `Resources`，用于查找本地化的字符串等。该类包含了 `ResourceManager` 和 `Culture` 属性，用于管理资源和文化信息。
    
    在 `Resources.resx` 文件中，添加了一个 XML 格式的资源文件，用于存储本地化的字符串和其他资源信息。
    
    在 `MainViewModel.cs` 文件中，添加了 `MainViewModel` 类，该类继承自 `ObservableObject`。该类包含两个可观察属性 `InputFontSize` 和 `TranslatedText`，以及一个异步方法 `Translate`，用于处理翻译请求。

--- TranslateWinUI3/View/MainView.xaml.cs
+++ TranslateWinUI3/View/MainView.xaml.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
+
 using Microsoft.UI.Xaml;
+
+using TranslateWinUI3.ViewModel;
 
 // To learn more about WinUI, the WinUI project structure, and more about our project templates,
 // see: http://aka.ms/winui-project-info.
@@ -10,9 +14,105 @@ namespace TranslateWinUI3.View
 	/// </summary>
 	public sealed partial class MainView : Window
 	{
+		public MainViewModel ViewModel { get; } = new MainViewModel();
+
 		public MainView()
 		{
 			this.InitializeComponent();
+
+			// 设置默认窗口大小
+			this.AppWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 900, Height = 1200 });
+
+			// 隐藏原生标题栏
+			this.ExtendsContentIntoTitleBar = true;
+
+			// 设置自定义标题栏
+			this.SetTitleBar(this.AppTitleBar);
+
+			// 设置 from 和 to 的默认语言
+			this.From.SelectedItem = this.From.Items[0];
+			this.To.SelectedItem = this.To.Items[0];
+		}
+
+		private void Switch_Tapped(object sender, RoutedEventArgs e)
+		{
+			if(this.To.SelectedItem == this.To.Items[0])
+			{
+				this.To.SelectedItem = this.To.Items[1];
+				this.From.SelectedItem = this.From.Items[2];
+			}
+			else
+			{
+				this.To.SelectedItem = this.To.Items[0];
+				this.From.SelectedItem = this.From.Items[1];
+			}
+		}
+
+		private async void Run_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+		{
+			string query = InputTextBox.Text;
+
+			if(string.IsNullOrEmpty(query))
+			{
+				return;
+			}
+
+			string form="";
+			string to="";
+
+			if(this.From.SelectedItem == this.From.Items[0])
+			{
+				form = "auto";
+			}
+			else if(this.From.SelectedItem == this.From.Items[1])
+			{
+				form = "en";
+			}
+			else if(this.From.SelectedItem == this.From.Items[2])
+			{
+				form = "zh";
+			}
+
+			if(this.To.SelectedItem == this.To.Items[0])
+			{
+				to = "zh";
+			}
+			else if(this.To.SelectedItem == this.To.Items[1])
+			{
+				to = "en";
+			}
+
+			await Task.Run(() =>
+			{
+				string result= "Translating..." + form + "|||" + to;
+				this.OutputTextBlock.Text = result;
+			});
+		}
+
+		private void From_SelectionChanged()
+		{
+			if(this.From.SelectedItem == this.From.Items[1])
+			{
+				this.To.SelectedItem = this.To.Items[0];
+			}
+
+			if(this.From.SelectedItem == this.From.Items[2])
+			{
+				this.To.SelectedItem = this.To.Items[1];
+			}
+		}
+
+		private void To_SelectionChanged()
+		{
+			if((this.From.SelectedItem != this.From.Items[0]) && (this.To.SelectedItem == this.To.Items[0]))
+			{
+				this.From.SelectedItem = this.From.Items[1];
+			}
+
+			if((this.From.SelectedItem != this.From.Items[0]) && (this.To.SelectedItem == this.To.Items[1]))
+			{
+				this.From.SelectedItem = this.From.Items[2];
+			}
 		}
 	}
 }
---
+++ TranslateWinUI3/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TranslateWinUI3.ViewModel
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		public MainViewModel()
+		{
+		}
+
+		[ObservableProperty]
+		private double inputFontSize = 22; // 初始字体大小
+
+		[ObservableProperty]
+		private string translatedText = string.Empty; // 翻译结果
+
+		internal async Task<bool> Translate(string query, string form, string to)
+		{
+			await Task.Run(() =>
+			{
+				TranslatedText = "Translating...";
+			});
+
+			return true;
+		}
+	}
+}
